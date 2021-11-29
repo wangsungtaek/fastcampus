5dc9ae5dfe3ddfc190978cf230e115ee4cb9c1e0
wangsungtaek <tjdxorv@naver.com>
2021-11-29 23:06:33 +0900

    first commit

---
+++ src/main/java/com/example/spring/novemer/NovemerApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.novemer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovemerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovemerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring/novemer/NovemerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.novemer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovemerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
