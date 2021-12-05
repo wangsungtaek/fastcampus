fd6a8694cd19b9cfc5fd5bea91fce3c6270b329f
wangsungtaek <tjdxorv@naver.com>
2021-12-06 00:12:05 +0900

    3강 05 완료

--- maker/src/main/java/com/fastcampus/programming/maker/controller/DMaKerController.java
+++ maker/src/main/java/com/fastcampus/programming/maker/controller/DMaKerController.java
@@ -1,5 +1,7 @@
 package com.fastcampus.programming.maker.controller;
 
+import com.fastcampus.programming.maker.service.DMakerService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +11,9 @@ import java.util.List;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class DMaKerController {
+    private final DMakerService dMakerService;
 
     @GetMapping("/developers")
     public List<String> getAllDevelopers() {
@@ -17,6 +21,17 @@ public class DMaKerController {
         log.info("GET /developers HTTP/1.1");
 
         return Arrays.asList("snow", "elsa", "Olaf");
+
+    }
+
+    @GetMapping("/create-developer")
+    public List<String> createDeveloper() {
+
+        log.info("GET /create-developer HTTP/1.1");
+
+        dMakerService.createDeveloper();
+
+        return List.of("Olaf");
 
     }
 }
--- maker/src/main/java/com/fastcampus/programming/maker/service/DMakerService.java
+++ maker/src/main/java/com/fastcampus/programming/maker/service/DMakerService.java
@@ -1,11 +1,28 @@
 package com.fastcampus.programming.maker.service;
 
+import com.fastcampus.programming.maker.entity.Developer;
 import com.fastcampus.programming.maker.repository.DeveloperRepository;
+import com.fastcampus.programming.maker.type.DeveloperLevel;
+import com.fastcampus.programming.maker.type.DeveloperSkillType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class DMakerService {
     private final DeveloperRepository developerRepository;
+
+    @Transactional
+    public void createDeveloper() {
+        Developer developer = Developer.builder()
+                .developerLevel(DeveloperLevel.JUNIOR)
+                .developerSkillType(DeveloperSkillType.FRONT_END)
+                .experienceYears(2)
+                .name("Olaf")
+                .age(5)
+                .build();
+
+        developerRepository.save(developer);
+    }
 }
